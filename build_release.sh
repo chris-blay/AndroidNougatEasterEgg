863af4cd5dcb117c914250882c465e6f632d4437
Chris Blay <chris.b.blay@gmail.com>
2018-01-18 21:20:40 -0800

    Release for V5
    Now with proguard map!

--- build_release.sh
+++ build_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2017 Christopher Blay <chris.b.blay@gmail.com>
+# Copyright (C) 2017, 2018 Christopher Blay <chris.b.blay@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 set -eu
 
 ANDROID_SDK=/opt/android-sdk
-BUILD_TOOLS_VERSION=26.0.1
+BUILD_TOOLS_VERSION=27.0.3
 
 if [ $# -ne 1 ]; then
     echo "Usage: $0 keystore"
@@ -33,12 +33,13 @@ fi
 
 cd workspace
 bazel build -c opt :AndroidNougatEasterEgg
-cp bazel-bin/AndroidNougatEasterEgg_unsigned.apk ..
+cp --no-preserve=mode bazel-bin/AndroidNougatEasterEgg_unsigned.apk ..
+cp --no-preserve=mode bazel-bin/AndroidNougatEasterEgg_proguard.map ..
 cd ..
 "$ANDROID_SDK/build-tools/$BUILD_TOOLS_VERSION/zipalign" -v -p 4 \
     AndroidNougatEasterEgg_unsigned.apk \
     AndroidNougatEasterEgg_unsigned_aligned.apk
-rm -f AndroidNougatEasterEgg_unsigned.apk
+rm AndroidNougatEasterEgg_unsigned.apk
 "$ANDROID_SDK/build-tools/$BUILD_TOOLS_VERSION/apksigner" sign \
     --ks "$KEYSTORE" --out AndroidNougatEasterEgg_signed.apk \
     AndroidNougatEasterEgg_unsigned_aligned.apk
